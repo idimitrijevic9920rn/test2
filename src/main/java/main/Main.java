4b7a9f61bbf0acf1c1451db174e4516bf6283f7a
dimitrijevich <107809349+dimitrijevich@users.noreply.github.com>
2022-11-25 15:16:58 +0100

    spec

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,6 +2,7 @@ package main;
 
 import repository.RepositoryImp;
 import specification.ProjectSpecification;
+import specification.StorageManager;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,7 +14,16 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        ProjectSpecification repositoryImp = new RepositoryImp();
+        try {
+//            Class.forName("repository.LocalRepositoryImp");
+            Class.forName("repository.Drive");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        ProjectSpecification sm = StorageManager.getStorageManager();
+
+        System.out.println("Welcome");
 
         String str;
         Scanner in = new Scanner(System.in);
@@ -28,97 +38,77 @@ public class Main {
 
             try {
                 if(parts[0].equals("makedir")){
-                    repositoryImp.initialiseDirectory(parts[1],parts[2],parts[3]);
+                    sm.initialiseDirectory(parts[1]);
                 }
 
-                if(parts[0].equals("changedir")){
-                    repositoryImp.changeDir(parts[1],parts[2],parts[3]);
-                }
-
-                if(parts[0].equals("login")){
-                    repositoryImp.logIn(parts[1],parts[2]);
-                }
-
-                if(parts[0].equals("logout")){
-                    repositoryImp.logOut();
-                }
 
                 if(parts[0].equals("makefile")){
-                    repositoryImp.createFile(parts[1],0);
+                    sm.createFile(parts[1],0);
                 }
+
                 if(parts[0].equals("makefiles")){
-                    repositoryImp.createFile(parts[1],1);
+                    sm.createFile(parts[1],1);
                 }
 
                 if(parts[0].equals("makefolder")){
-                    repositoryImp.createStorage(parts[1],0);
+                    sm.createStorage(parts[1],0);
                 }
 
                 if(parts[0].equals("makefolders")){
-                    repositoryImp.createStorage(parts[1],1);
+                    sm.createStorage(parts[1],1);
                 }
 
-                if(parts[0].equals("deletefile")){
-                    repositoryImp.deleteFile(parts[1],0);
+                //delete folder1/file.txt ili ako brisemo ceo folder delete folder1
+                if(parts[0].equals("delete")){
+                    sm.deleteFile(parts[1],1);
                 }
 
+//                "deletefiles folder1/f1.txt,folder1/f2.txt"
                 if(parts[0].equals("deletefiles")){
-                    repositoryImp.deleteFile(parts[1],1);
+                    sm.deleteFile(parts[1],2);
                 }
 
-                if(parts[0].equals("deleteall")){
-                    repositoryImp.deleteFile(parts[1],2);
-                }
-
+//               "move folder1/mm.txt folder2"
                 if(parts[0].equals("move")){
-                    repositoryImp.moveFile(parts[1],parts[2]);
+                    sm.moveFile(parts[1],parts[2]);
                 }
 
-                if(parts[0].equals("makeuser")){
-                    String[] tokens = parts[4].split(",");
-                    repositoryImp.createUser(parts[1],parts[2],parts[3], Arrays.asList(tokens));
-                }
 
                 if(parts[0].equals("download")){
-                    repositoryImp.downloadFile(parts[1]);
+                    sm.downloadFile(parts[1]);
                 }
 
                 if(parts[0].equals("makeconfig")){
-                    String[] folders = parts[2].split(",");
-                    String[] nums = parts[3].split(",");
-                    List<Integer> ints = new ArrayList<>();
+
+                    String[] folders = parts[1].split(",");
+
+                    String[] nums = parts[2].split(",");
+                    List<Integer> maxLens = new ArrayList<>();
                     for(String num:nums){
-                        ints.add(Integer.valueOf(num));
+                        maxLens.add(Integer.valueOf(num));
                     }
-                    String[] filetypes = parts[4].split(",");
-                    repositoryImp.createConfig(Integer.valueOf(parts[1]),Arrays.asList(folders),ints,Arrays.asList(filetypes));
+
+
+                    String[] filetypes = parts[3].split(",");
+
+                    List<Integer> intMaxSizes = new ArrayList<>();
+                    String[] maxSize = parts[4].split(",");
+                    for(String s:maxSize){
+                        intMaxSizes.add(Integer.valueOf(s));
+                    }
+
+                    sm.createConfig(Arrays.asList(folders),maxLens,Arrays.asList(filetypes),intMaxSizes);
+
                 }
 
                 if(parts[0].equals("list")){
-                    if(parts[1].equals("filesdir"))
-                        repositoryImp.viewFile(parts[2],1);
-                    if(parts[1].equals("filesdirsubdir"))
-                        repositoryImp.viewFile(parts[2],2);
-                    if(parts[1].equals("foldersdirsubdir"))
-                        repositoryImp.viewFile(parts[2],3);
-                    if(parts[1].equals("foldersdir"))
-                        repositoryImp.viewFile(parts[2],4);
-                    if(parts[1].equals("sortedfiles"))
-                        repositoryImp.viewFile(parts[2],5);
-                    if(parts[1].equals("sortedfolders"))
-                        repositoryImp.viewFile(parts[2],6);
-                    if(parts[1].equals("filesbydatecreated"))
-                        repositoryImp.viewFile(parts[2],7);
-                    if(parts[1].equals("filesbydatemodified"))
-                        repositoryImp.viewFile(parts[2],8);
-                }
-
-                if(parts[0].equals("upload")){
-
+                    System.out.println("prikazi");
+//
                 }
 
 
             }catch (IndexOutOfBoundsException e){
+                System.out.println(e);
                 System.out.println("wrong syntax");
             }
 
